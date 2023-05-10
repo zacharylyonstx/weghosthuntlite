b88bd6fa6578cf1f3bcf876475f06238a4a6fa7d
Zak Lyons <zacharylyonstx@gmail.com>
2023-05-10 02:31:47 -0500

    **WIP:**  This commit adds a new yarn workspace configuration among other project-configuration additions, it also contains completely new chart components components with old components removed and a revised README.md
    ## In this commit:
    
    -  Append `craco.config.js` for css less
    
    - Update css files for new theme
    
    - Remove previous chart components in favor of newer components
    
    - Add yarn workspaces to root directory
    
    - add react-redux package, configured `store.js` & implemented its use for the CSV data
    
    - add .vscode dir/settings to begin working in standard dev environments

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,5 +1,6 @@
 /* craco.config.js */
 const path = require('path');
+const CracoLessPlugin = require('craco-less');
 
 module.exports = {
     webpack: {
@@ -7,7 +8,27 @@ module.exports = {
             '@app': path.resolve(__dirname, 'src'),
             'components': path.resolve(__dirname, 'src/components'),
             'pages': path.resolve(__dirname, 'src/pages'),
-            
         },
-    }
+        plugins: {
+            add: [
+                CracoLessPlugin({
+                    lessLoaderOptions: {
+                        lessOptions: {
+                            modifyVars: {
+                                '@primary-color': '#28B2F0',
+                                '@link-color': '#28B2F0',
+                                '@border-radius-base': '2px',
+                                '@text-color': '#D0D0D0',
+                                '@text-color-secondary': '#A0A0A0',
+                                '@layout-header-background': '#202020',
+                                '@layout-body-background': '#151515',
+                                '@layout-footer-background': '#202020',
+                            },
+                            javascriptEnabled: true,
+                        },
+                    },
+                })
+            ]
+        },
+    },
 };
